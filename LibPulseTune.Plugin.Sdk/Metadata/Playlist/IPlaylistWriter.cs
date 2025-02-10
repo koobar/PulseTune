dc2452e2c278bddeb7eacfaf6d6f4893c44bc8fb
koobar <koobar14@gmail.com>
2025-02-10 15:29:19 +0900

    不要となったファイルを削除

--- LibPulseTune.Plugin.Sdk/Metadata/Playlist/IPlaylistWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibPulseTune.Plugin.Sdk.Metadata.Playlist
-{
-    public interface IPlaylistWriter
-    {
-        void OpenFile(string path);
-
-        void Write(Playlist playlist);
-    }
-}
